7eca76acae93a2b806c617a28b88d3f041bdd312
Dave Davis <dave@redflymarketing.com>
2018-03-08 05:01:06 +0000

    Add OnClickListners to main menu items to open respective activities.

--- app/src/main/java/com/echofly/babatrack/MainActivity.java
+++ app/src/main/java/com/echofly/babatrack/MainActivity.java
@@ -5,25 +5,66 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        TextView pinkNoiseTextView = findViewById(R.id.pinknoise);
+        TextView whiteNoiseTextView = findViewById(R.id.whitenoise);
+        TextView natureTextView = findViewById(R.id.nature);
+        TextView weatherTextView = findViewById(R.id.weather);
+
+        //Todo: Use switch or investigate ways to make this more efficient without having to duplicate onClickListner code.
+
+        //Set the Pink Noise OnClickListener and use an Intent to open the Pink Noise Activity
+        pinkNoiseTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pinkNoiseIntent = new Intent(MainActivity.this, PinkNoiseActivity.class);
+                startActivity(pinkNoiseIntent);
+            }
+        });
+
+
+        //Set the White Noise OnClickListener and use an Intent to open the White Noise Activity
+        whiteNoiseTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent whiteNoiseIntent = new Intent(MainActivity.this, WhiteNoiseActivity.class);
+                startActivity(whiteNoiseIntent);
+            }
+        });
+
+        //Set the Nature OnClickListener and use an Intent to open the Nature Activity
+        natureTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent natureIntent = new Intent(MainActivity.this, NatureActivity.class);
+                startActivity(natureIntent);
+            }
+        });
+
+
+        //Set the Weather OnClickListener and use an Intent to open the Pink Noise Activity
+        weatherTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent weatherIntent = new Intent(MainActivity.this, WeatherActivity.class);
+                startActivity(weatherIntent);
+            }
+        });
+
+
+
+
+
     }
-
-
-        public void openPinkNoiseList(View view){
-            Intent pinkNoiseIntent = new Intent(this, PinkNoiseActivity.class);
-            startActivity(pinkNoiseIntent);
-
-        }
-
-
 
 
 }
