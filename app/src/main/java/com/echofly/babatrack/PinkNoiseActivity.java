d8aedad4d10c54dad984792dd90848c1a490e893
Dave Davis <davedavis@users.noreply.github.com>
2018-03-09 05:10:30 +0000

    Add Pink Noise data in the form of an ArrayList and loop over the ArrayList to populate the data from the ArrayList into dynamically added  TextViews.

--- app/src/main/java/com/echofly/babatrack/PinkNoiseActivity.java
+++ app/src/main/java/com/echofly/babatrack/PinkNoiseActivity.java
@@ -1,7 +1,11 @@
 package com.echofly.babatrack;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class PinkNoiseActivity extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class PinkNoiseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pink_noise);
+
+        // Todo: Make these hardcoded
+        // Initializing a string array to store the sound names. These could be hard coded in the
+        // TextView but I'm following along with the course so want to do these this way.
+        ArrayList<String> pinkNoises = new ArrayList<String>();
+        // Adding the names of the sounds to the string array.
+        pinkNoises.add("Absorption");
+        pinkNoises.add("Celestial");
+        pinkNoises.add("Escape Velocity");
+        pinkNoises.add("Heliocentric");
+        pinkNoises.add("Lithosphere");
+        pinkNoises.add("Parabola");
+        pinkNoises.add("Quasar");
+        pinkNoises.add("Triton");
+        pinkNoises.add("Ultraviolet");
+        pinkNoises.add("Zenith");
+
+
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        for (int i = 0; i < pinkNoises.size(); i++) {
+            TextView pinkNoisesTextView = new TextView(this);
+            pinkNoisesTextView.setText(pinkNoises.get(i));
+            rootView.addView(pinkNoisesTextView);
+        }
+
+
     }
 }
